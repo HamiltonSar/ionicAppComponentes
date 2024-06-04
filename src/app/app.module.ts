2c757438d640a4f85edc9aafd3e4f9f502a9766b
hamilton <hamiltonsarmientopacheco.com>
2024-06-03 16:06:21 -1200

    Primera Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+//import { AppHeader from './app.component'; 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AvatarComponent } from './pages/avatar/avatar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent , AvatarComponent ],
+
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+ // exports:[AppHeaderComponent],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
